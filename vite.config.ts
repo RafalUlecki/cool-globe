e9153ce6c2397a42c36902dae8fe7e022e167ad5
Rafal <uleckirafal@gmail.com>
2026-05-09 17:05:24 +0300

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  define:
+    command === "build"
+      ? { "process.env.NODE_ENV": JSON.stringify("production") }
+      : undefined,
   build: {
     copyPublicDir: false,
     lib: {
@@ -12,7 +16,13 @@ export default defineConfig({
       formats: ["es", "cjs"],
     },
     rollupOptions: {
-      external: ["react", "react-dom", "react-globe.gl"],
+      external: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+        "react/jsx-dev-runtime",
+        "react-globe.gl",
+      ],
     },
   },
-});
+}));
